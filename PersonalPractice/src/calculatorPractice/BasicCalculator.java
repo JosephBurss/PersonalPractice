15f52bd79881331fec802ffc0b7c9e4b61646d1c
jburs <josephburss97@gmail.com>
2023-11-11 19:59:37 -0600

    implemented add and subtract

--- PersonalPractice/src/calculatorPractice/BasicCalculator.java
+++ PersonalPractice/src/calculatorPractice/BasicCalculator.java
@@ -1,17 +1,62 @@
 package calculatorPractice;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class BasicCalculator {
 	
+	public static void main(String[] args) throws IOException {
+		
+		performOperation();
+
+	}
 	
-	public static int add(int addend1, int addend2) {
+	public static int add() throws IOException {
+		
+		// Enter data using BufferReader
+        BufferedReader reader = new BufferedReader(
+            new InputStreamReader(System.in));
+        
+		System.out.println("please enter the first number to add");
+		// Reading data using readLine
+        String input = reader.readLine();
+        int addend1 = Integer.parseInt(input);
+        
+        System.out.println("please enter the second number to add");
+		// Reading data using readLine
+        input = reader.readLine();
+        int addend2 = Integer.parseInt(input);
 		
 		int result = addend1 + addend2;
+		System.out.println("The sum of this addition is " + result);
+		System.out.println("//");
+		System.out.println("//");
+		System.out.println("//");
 		return result;
 	}
 	
-	public static int subtract(int minuend, int subtrahend) {
+	public static int subtract() throws IOException {
+		
+		// Enter data using BufferReader
+        BufferedReader reader = new BufferedReader(
+            new InputStreamReader(System.in));
+        
+		System.out.println("please enter your first number");
+		// Reading data using readLine
+        String input = reader.readLine();
+        int minuend = Integer.parseInt(input);
+        
+        System.out.println("please enter the number to subtract");
+		// Reading data using readLine
+        input = reader.readLine();
+        int subtrahend = Integer.parseInt(input);
 		
 		int result = minuend - subtrahend;
+		System.out.println("The difference is " + result);
+		System.out.println("//");
+		System.out.println("//");
+		System.out.println("//");
 		return result;
 	}
 	
@@ -27,9 +72,61 @@ public class BasicCalculator {
 		return result;
 	}
 	
-	public static void main(String[] args){
+	public static void performOperation() throws IOException {
 		
-		System.out.println("test");
+		// Enter data using BufferReader
+        BufferedReader reader = new BufferedReader(
+            new InputStreamReader(System.in));
+			
+		System.out.println("what operation do you want to perform? add - subtract - multiply - divide");
+		System.out.println("//");
+		System.out.println("//");
+		System.out.println("//");
+		System.out.println("please enter one of the following a , s , m , d");
 		
+		// Reading data using readLine
+        String operationInput = reader.readLine();
+        
+        switch (operationInput)
+        {
+            case "a":
+            	System.out.println("addition selected");
+            	add();
+            	performOperation();
+	            break;
+			case "s":
+            	System.out.println("subtraction selected");
+            	subtract();
+            	performOperation();
+            	break;            
+            case "m":
+            	System.out.println("multiplicaion selected");
+            	break;            
+            case "d":
+            	System.out.println("division selected");
+            	break;            
+            default:
+                System.out.println("Invalid opertaion, please try again");
+                performOperation();
+        }
 	}
+	
+
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
