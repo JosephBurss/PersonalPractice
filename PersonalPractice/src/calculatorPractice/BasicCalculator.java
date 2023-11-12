328ccfddb991821b9530be684b10f34b25e37bc1
jburs <josephburss97@gmail.com>
2023-11-11 19:08:54 -0600

    init commit

---
+++ PersonalPractice/src/calculatorPractice/BasicCalculator.java
@@ -0,0 +1,16 @@
+package calculatorPractice;
+
+public class BasicCalculator {
+	
+	
+	public static int add(int addend1, int addend2) {
+		
+		int result = addend1 + addend2;
+		return result;
+	}
+	
+	public static void main(String[] args){
+
+		
+	}
+}
