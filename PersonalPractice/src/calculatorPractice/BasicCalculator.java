863f7e9a923ac4b68ce136e38f4588b91797b3cf
jburs <josephburss97@gmail.com>
2023-11-11 19:20:39 -0600

    added subtract, multiply, divide

--- PersonalPractice/src/calculatorPractice/BasicCalculator.java
+++ PersonalPractice/src/calculatorPractice/BasicCalculator.java
@@ -9,6 +9,24 @@ public class BasicCalculator {
 		return result;
 	}
 	
+	public static int subtract(int minuend, int subtrahend) {
+		
+		int result = minuend - subtrahend;
+		return result;
+	}
+	
+	public static int multiply(int multiplicand, int multiplier) {
+		
+		int result = multiplicand * multiplier;
+		return result;
+	}
+	
+	public static int divide(int dividend, int divisor) {
+		
+		int result = dividend / divisor;
+		return result;
+	}
+	
 	public static void main(String[] args){
 
 		
